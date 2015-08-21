bcaea51c4454f499f8fc8618a670b9e8f0580990
dusensong <58247052@qq.com>
2015-08-21 15:08:50 +0800

    add utils

--- app/src/main/java/cn/wehax/common/util/AppUtils.java
+++ app/src/main/java/cn/wehax/common/util/AppUtils.java
@@ -2,8 +2,6 @@ package cn.wehax.common.util;
 
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
-import android.app.ActivityManager.RunningTaskInfo;
-import android.content.ComponentName;
 import android.content.Context;
 
 import java.util.List;
@@ -58,6 +56,49 @@ public class AppUtils {
     }
 
     /**
+     * 检测指定App是否在运行
+     *
+     * @param context     使用本程序的Activity或Service
+     * @param PackageName 指定要检查的App的包名
+     * @return 如果指定android程序正在运行，返回true
+     */
+    public static boolean isAppRunning(Context context, String PackageName) {
+        boolean isRuning = false;
+        try {
+            ActivityManager mActivityManager =
+                    (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<RunningAppProcessInfo> appProcesses = mActivityManager.getRunningAppProcesses();
+            for (RunningAppProcessInfo appProcess : appProcesses) {
+                if (appProcess.processName.equals(PackageName)) {
+                    isRuning = true;
+                    break;
+                }
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return isRuning;
+    }
+
+    /**
+     * 检测一个Service是否在运行
+     *
+     * @param context
+     * @param serviceClassName 待检测的服务的类名（完全限定名：包名+服务类名）
+     * @return 如果指定Service正在运行，返回true
+     */
+    public static boolean isServiceRunning(Context context, String serviceClassName) {
+        ActivityManager manager =
+                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClassName.equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * whether application is in background
      * <ul>
      * <li>need use permission android.permission.GET_TASKS in Manifest.xml</li>
@@ -66,15 +107,52 @@ public class AppUtils {
      * @param context
      * @return if application is in background return true, otherwise return false
      */
-    public static boolean isApplicationInBackground(Context context) {
-        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<RunningTaskInfo> taskList = am.getRunningTasks(1);
-        if (taskList != null && !taskList.isEmpty()) {
-            ComponentName topActivity = taskList.get(0).topActivity;
-            if (topActivity != null && !topActivity.getPackageName().equals(context.getPackageName())) {
+    public static boolean isAppInBackground(Context context) {
+        // Returns a list of application processes that are running on the device
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        String packageName = context.getPackageName();
+
+        List<RunningAppProcessInfo> appProcesses = activityManager
+                .getRunningAppProcesses();
+
+        if (appProcesses == null)
+            return false;
+
+        for (RunningAppProcessInfo appProcess : appProcesses) {
+            // The name of the process that this object is associated with.
+            if (appProcess.processName.equals(packageName)
+                    && appProcess.importance == RunningAppProcessInfo.IMPORTANCE_BACKGROUND) {
                 return true;
             }
         }
+
+        return false;
+    }
+
+    /**
+     * 程序是否在前台运行
+     *
+     * @return if application is on foreground return true, otherwise return false
+     */
+    public boolean isAppOnForeground(Context context) {
+        // Returns a list of application processes that are running on the device
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        String packageName = context.getPackageName();
+
+        List<RunningAppProcessInfo> appProcesses = activityManager
+                .getRunningAppProcesses();
+
+        if (appProcesses == null)
+            return false;
+
+        for (RunningAppProcessInfo appProcess : appProcesses) {
+            // The name of the process that this object is associated with.
+            if (appProcess.processName.equals(packageName)
+                    && appProcess.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
